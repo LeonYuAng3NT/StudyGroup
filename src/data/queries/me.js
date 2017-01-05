7fcab14e692468e4dc05d764cb520ef80eaf3502
LeonYuAng3NT <leonzhang1996@hotmail.com>
2017-01-05 15:38:50 -0500

    schema changes

--- src/data/queries/me.js
+++ src/data/queries/me.js
@@ -14,9 +14,9 @@ const User = {
   resolve({ request }) {
     return request.user && {
       id: request.user.id,
-      email: request.user.email,
+      email: request.user.email
     };
-  },
+  }
 };
 
 export default User;
